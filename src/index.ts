06ce189f96918595b76275f47af82d43b1ef64cf
Mateus Pimentel <mateus.pimentel.w@gmail.com>
2022-07-06 17:16:55 -0300

    Add code on index.ts to test it on dev server + signint to stop

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,26 @@ import calculateSlot from "cluster-key-slot";
 
 
 import {readFile as _readFile, writeFileSync} from "fs";
+import {DownTimeCheckerWorker} from "./downTimeChecker";
 const readFile = promisify(_readFile)
 
 let run = async () => {
+    let cluster = new RedisCluster({host: "test-cluster-2.pi8pe7.clustercfg.usw2.cache.amazonaws.com", port: 6379});
+    let downtimeWatcher = new DownTimeCheckerWorker(
+        cluster,
+        {},
+        "22-07-06:redis-tool-test-16:15",
+        1000 * 60 * 60,
+        1000,
+        50
+    )
+
+    downtimeWatcher.start();
+
+    process.on('SIGINT', async function() {
+        await downtimeWatcher.stop();
+        process.exit();
+    });
 
 }
 
