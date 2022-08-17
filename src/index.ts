361abce1a51f4070ef94f76a93f9a5a2c11ea529
Mateus Pimentel <mateus.pimentel.w@gmail.com>
2022-08-17 14:57:11 -0300

    Add redis-tool to npm script

--- src/index.ts
+++ src/index.ts
@@ -14,10 +14,11 @@ program
     .argument('eventId', 'Event Id')
     .option('--safe', 'Safe mode - sets the ttl of keys to 30 days or value specified in --ttl instead of deleting them')
     .option('--ttl <value>', 'TTL value for safe mode')
-    .option('-h <value>', 'Host')
-    .option('-p <value>', 'Port')
+    .requiredOption('-h, --host <value>', 'Host')
+    .requiredOption('-p, --port <value>', 'Port')
     .action(async (eventId, options) => {
-        let cluster = new RedisCluster({host: options.h, port: options.p});
+
+        let cluster = new RedisCluster({host: options.host, port: options.port});
 
         // options is being passed directly into the worker here, we are not validating it for now.
         let worker = new DeleteLeaderboardWorker(cluster, eventId, options);
